fd27edaec78f7d4633536c7d396b0612e48cedd8
Nicola Galeotti <niccio2802@outlook.it>
2020-11-13 19:17:25 +0100

    aggiunta console app di prova

---
+++ Prova/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Ereditarietà_1;
+
+namespace Prova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Docente d = new Docente("Arminio", "Aristogatti", DateTime.Now, 1.70, 7000, "Matematica");
+            Console.WriteLine(d.GetNomeCompleto() + "\n" + d.GetDataDiNascita() + "\n" + d.GetAltezza() + "\n" + d.GetStipendio() + "\n" + d.GetMateria());
+            Console.ReadLine();
+        }
+    }
+}
